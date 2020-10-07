066ef65a7d27436bf69b86f2efdd097111e47591
unknown <haasin.farooq@gmail.com>
2020-10-07 22:33:23 +0500

    created nav-bar, book-list and theme-toggle components, created theme and auth context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import NavBar from './components/nav-bar.component';
+import BookList from './components/book-list.component';
+import ThemeToggle from './components/theme-toggle.component';
+
+import ThemeContextProvider from './contexts/theme.context';
+import AuthContextProvider from './contexts/auth.context';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeContextProvider>
+        <AuthContextProvider>
+          <NavBar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
+      </ThemeContextProvider>
     </div>
   );
 }
---
+++ src/components/book-list.component.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+import { ThemeContext } from '../contexts/theme.context';
+
+class BookList extends Component {
+    static contextType = ThemeContext;
+    render() {
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
+        return (
+            <div className="book-list" style={{ background: theme.bg, color: theme.syntax }}>
+                <ul>
+                    <li style={{ background: theme.ui }}>The Way of Kings</li>
+                    <li style={{ background: theme.ui }}>The Name of the Wind</li>
+                    <li style={{ background: theme.ui }}>The Final Empire</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default BookList;
